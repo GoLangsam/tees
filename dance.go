e0e218e1f93ea0d42b124200c528328462dbb1f9
GoLangsam <GoLangsam.APA@a-bc.net>
2018-10-28 05:40:04 +0200

    shorter

--- dance.go
+++ dance.go
@@ -9,30 +9,30 @@ import (
 )
 
 // ========================================================
+// A Dancer loves dancing his Dance.
 type Dancer interface {
 	Away() *list.Element
 	ForEachNext(f func(*list.Element))
 	ForEachPrev(f func(*list.Element))
-	Root() *list.Element
 }
 
 // ========================================================
 
-// Dance l is where the dancing begins
-func Dance(l Dancer, d *list.Dancing) {
-	unC(l)
-	dance(l.Root(), d)
-	reC(l)
+// Dance e is where the dancing begins.
+func Dance(e Dancer, d *list.Dancing) {
+	unC(e)		//   fold Col
+	dance(e, d)	// dance dance dance
+	reC(e)		//   open Col
 }
 
 // dance e is where the dancing continues
 func dance(e Dancer, d *list.Dancing) {
 	ForEachNext(e, func(i *list.Element) {
-		d.OnGoal(i) // Publish candidate
-		unR(i.Away())
-		d.Dance() // Dance d is where the dancing recurs to
-		reR(i.Away())
-		d.OnFail() // Pop
+		d.OnGoal(i)	// Publish candidate
+		unR(i.Away())	//   fold Row
+		d.Dance()	// Dance d is where the dancing recurs to
+		reR(i.Away())	//   open Roe
+		d.OnFail()	// Plopp :-(
 	})
 }
 
@@ -40,25 +40,14 @@ func dance(e Dancer, d *list.Dancing) {
 func unC(c Dancer) { c.Away().UnLink(); unL(c) }
 func reC(c Dancer) { c.Away().ReLink(); reL(c) }
 
-func unR(r Dancer) { ForEachNext(r, unAf) }
-func reR(r Dancer) { ForEachPrev(r, reAf) }
+func unR(r Dancer) { ForEachNext(r, func(i *list.Element) { unC(i.Away().List()) }) }
+func reR(r Dancer) { ForEachPrev(r, func(i *list.Element) { reC(i.Away().List()) }) }
 
-func unL(l Dancer) { ForEachNext(l, unEf) }
-func reL(l Dancer) { ForEachPrev(l, reEf) }
+func unL(l Dancer) { ForEachNext(l, func(i *list.Element) { unE(i.Away()) }) }
+func reL(l Dancer) { ForEachPrev(l, func(i *list.Element) { reE(i.Away()) }) }
 
-func unE(e Dancer) { ForEachNext(e, unKf) }
-func reE(e Dancer) { ForEachPrev(e, reKf) }
-
-// ========================================================
-
-var unAf = func(i *list.Element) { unC(i.Away().List()) }
-var reAf = func(i *list.Element) { reC(i.Away().List()) }
-
-var unEf = func(i *list.Element) { unE(i.Away()) }
-var reEf = func(i *list.Element) { reE(i.Away()) }
-
-var unKf = func(i *list.Element) { i.Away().UnLink() }
-var reKf = func(i *list.Element) { i.Away().ReLink() }
+func unE(e Dancer) { ForEachNext(e, func(i *list.Element) { i.Away().UnLink() }) }
+func reE(e Dancer) { ForEachPrev(e, func(i *list.Element) { i.Away().ReLink() }) }
 
 // ========================================================
 
