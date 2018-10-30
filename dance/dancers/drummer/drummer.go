8f30a57a250ebc9c00b2df3f51b68f756f452a7c
GoLangsam <GoLangsam.APA@a-bc.net>
2018-10-30 10:44:04 +0200

    new dancer: drummer

---
+++ dance/dancers/drummer/drummer.go
@@ -0,0 +1,47 @@
+// Copyright 2016 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package drummer
+
+import (
+	"github.com/GoLangsam/tees/dance/dancers/drummer/drum" // == "github.com/GoLangsam/container/oneway/drum"
+)
+
+type Drums struct {
+	Goal *drum.Drum // Niveaus counts dances per Level
+	Fail *drum.Drum // Deadend counts fail ends per Level
+	Call *drum.Drum // Grooves counts solutions per length
+	Leaf *drum.Drum // UpDates counts unLink per Level
+	// Note: No drum for Push & Pop, as these occur in sync with Call
+}
+
+func NewDrums(cap int, verbose bool) *Drums {
+	var d = new(Drums)
+	d = d.init(cap, verbose)
+	return d
+}
+
+func (d *Drums) init(cap int, verbose bool) *Drums {
+	d.Goal = drum.NewDrum("Grooves", cap)
+	d.Fail = drum.NewDrum("Deadend", cap)
+	d.Call = drum.NewDrum("Niveaus", cap)
+	d.Leaf = drum.NewDrum("UpDates", cap)
+
+	if verbose {
+		drum.Verbose = true
+	} else {
+		drum.Verbose = false
+	}
+
+	return d
+}
+
+func (d *Drums) Print() {
+	if Verbose {
+		d.Goal.Print()
+		d.Fail.Print()
+		d.Call.Print()
+		d.Leaf.Print()
+	}
+}
