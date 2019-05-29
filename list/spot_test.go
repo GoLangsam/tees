ef64cebc9ae58a605dc1b91f9f4a48cb7cb71fb6
GoLangsam <GoLangsam.APA@a-bc.net>
2019-05-29 20:32:00 +0630

    restructure interface tests

--- list/spot_test.go
+++ list/spot_test.go
@@ -8,14 +8,15 @@ import (
 	"github.com/GoLangsam/tees/list"
 )
 
-var _ Spot = list.New()
-var _ Spot = list.New().Root()
+func Example_spot() {
+	var _ Spot = list.New()
+	var _ Spot = list.New().Root()
+}
 
 // Spot abstracts the common behaviour of *list.Element & *list.List
 type Spot interface {
+	node
 	Away
-	Beam
-	Dust
 	Sama
 }
 
@@ -44,41 +45,6 @@ type AwayDust interface {
 	Away() *list.Element
 	IsJunk() bool
 	IsSolo() bool
-}
-
-// Beam abstracts the 'lengthy' behaviour common to *list.Element & *list.List
-type Beam interface {
-	CanIter // Front Next
-	CanReti // Back  Prev
-	Len() int
-	Root() *list.Element
-}
-
-// CanIter allows to iterate forward by starting with Front() and, if non-nil, repeating Next() until Next() returns nil
-type CanIter interface {
-	Front() *list.Element
-	Next() *list.Element
-	ForEachNext(f func(*list.Element))
-}
-
-// CanReti allows to iterate backward by starting with Back() and, if non-nil, repeating Prev() until Prev() returns nil
-//  Note: Reti is Iter spelled backwards.
-type CanReti interface {
-	Back() *list.Element
-	Prev() *list.Element
-	ForEachPrev(f func(*list.Element))
-}
-
-// Dust abstracts the 'pointy' behaviour common to *list.Element & *list.List
-type Dust interface {
-	CVs() *list.ComposedValue
-
-	IsAtom() bool
-	IsComposed() bool
-
-	PrintAtomValues(args ...interface{})
-	PrintValue(args ...interface{})
-	//	Values() list.Values
 }
 
 // Sama abstracts the 'dancing' behaviour common to *list.Element & *list.List
--- list/spotlist_test.go
+++ list/spotlist_test.go
@@ -8,8 +8,10 @@ import (
 	"github.com/GoLangsam/tees/list"
 )
 
-var _ Nova = list.New()
-var _ Node = list.New().Root()
+func Example_nova() {
+	var _ Nova = list.New()
+	var _ Node = list.New().Root()
+}
 
 // Nova is a spot which can create new elements and Remove existing ones
 // and thus abstracts common behaviour of *list.List and *anda.Anda, Aton, Apep ...
@@ -22,43 +24,4 @@ type Nova interface {
 type Node interface {
 	Spot
 	Atom
-}
-
-// Coll combines all methods unique to any list, and not shared with Element
-type Coll interface {
-	Init() *list.List
-//	Clear() *list.List
-
-	Remove(*list.Element) interface{}
-
-	IsEmpty() bool
-
-	Elements() []*list.Element
-	Values() list.Values
-
-	Print(args ...interface{})
-
-	InsertAfter(v interface{}, mark *list.Element) *list.Element
-	InsertBefore(v interface{}, mark *list.Element) *list.Element
-
-	MoveAfter(e, mark *list.Element)
-	MoveBefore(e, mark *list.Element)
-	MoveToBack(e *list.Element)
-	MoveToFront(e *list.Element)
-
-	PushFront(v interface{}) *list.Element
-	PushBack(v interface{}) *list.Element
-	PushBackList(other *list.List)
-	PushFrontList(other *list.List)
-
-	ValuesPushFront(values ...interface{})
-	ValuesPushBack(values ...interface{})
-}
-
-// Atom combines all methods unique to any element, and not shared with List
-type Atom interface {
-	IsNode() bool
-	IsRoot() bool
-
-	List() *list.List
 }
