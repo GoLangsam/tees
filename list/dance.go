916ed46081ae60ce8d714cee6e39359b1c0d710e
GoLangsam <GoLangsam.APA@a-bc.net>
2019-06-29 22:52:55 +0630

    allow for ring in dance.go

--- list/dance.go
+++ list/dance.go
@@ -32,14 +32,14 @@ func (e *Element)  Dance( d *Dancing ) {
 }
 
 // ========================================================
-func (l *List)      fold( d *Dancing ) { 						l.root.away.UnLeaf(d);	l.unList(d)		    }
-func (l *List)      open( d *Dancing ) { 						l.root.away.ReLeaf(d);	l.reList(d)		    }
+func (l *List)      fold( d *Dancing ) { 										l.root.away.UnLeaf(d);	l.unList(d)	}
+func (l *List)      open( d *Dancing ) { 										l.root.away.ReLeaf(d);	l.reList(d)	}
 
-func (e *Element)   fold( d *Dancing ) { for i := e.next; i != e; i = i.next { if i != &e.list.root {	i.away.list.fold(d)	} } }
-func (e *Element)   open( d *Dancing ) { for i := e.prev; i != e; i = i.prev { if i != &e.list.root {	i.away.list.open(d)	} } }
+func (e *Element)   fold( d *Dancing ) { for i := e.next; i != e; i = i.next { if i.list == nil || i != &e.list.root {	i.away.list.fold(d)	} } }
+func (e *Element)   open( d *Dancing ) { for i := e.prev; i != e; i = i.prev { if i.list == nil || i != &e.list.root {	i.away.list.open(d)	} } }
 
-func (l *List)    unList( d *Dancing ) { for i := l.root.next; i != &l.root; i = i.next { 		i.away.unList(d)	  } }
-func (l *List)    reList( d *Dancing ) { for i := l.root.prev; i != &l.root; i = i.prev { 		i.away.reList(d)	  } }
+func (l *List)    unList( d *Dancing ) { for i := l.root.next; i != &l.root; i = i.next { 				i.away.unList(d)	  } }
+func (l *List)    reList( d *Dancing ) { for i := l.root.prev; i != &l.root; i = i.prev { 				i.away.reList(d)	  } }
 
-func (e *Element) unList( d *Dancing ) { for i := e.next; i != e; i = i.next { if i != &e.list.root {	i.away.UnLeaf(d)	} } }
-func (e *Element) reList( d *Dancing ) { for i := e.prev; i != e; i = i.prev { if i != &e.list.root {	i.away.ReLeaf(d)	} } }
+func (e *Element) unList( d *Dancing ) { for i := e.next; i != e; i = i.next { if i.list == nil || i != &e.list.root {	i.away.UnLeaf(d)	} } }
+func (e *Element) reList( d *Dancing ) { for i := e.prev; i != e; i = i.prev { if i.list == nil || i != &e.list.root {	i.away.ReLeaf(d)	} } }
